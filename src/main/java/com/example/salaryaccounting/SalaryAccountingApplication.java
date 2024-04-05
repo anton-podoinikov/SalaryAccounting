7dd2fdee7a6fccf00a33a925af8e2dda24906550
Anton Podoinikov <antpkov@gmail.com>
2024-04-05 18:30:15 +0300

    Create project SalaryAccounting. It is first commit.

---
+++ src/main/java/com/example/salaryaccounting/SalaryAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.example.salaryaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryAccountingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salaryaccounting/SalaryAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salaryaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryAccountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
